daa564ec669b8ef84a0d317f44053101ecc6fb8b
Shimizu <yu.shimizu@makip.co.jp>
2025-03-27 15:45:23 +0900

    ESLint設定の更新とPrettierプラグインの追加

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
+  eslintPluginPrettierRecommended,
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
